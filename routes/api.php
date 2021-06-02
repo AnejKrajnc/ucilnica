fc30c9cd5d71f8bfa15b45f30346a6023d511dea
AnejKrajnc <anej.krajnc@hotmail.com>
2021-06-02 09:51:15 +0200

    Added API routes
    Added API routes for dynamic adding and removing users and courses enrolled by specific users

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::middleware('api')->post('/prikazi-vsebino', 'Courses\CourseModuleContentsController@returnContent');
+
+//Api roeutes for dynamic adding and removing users and courses enrolled by specific user
+Route::middleware(['api', 'admin'])->get('/profile', 'AdminController@profile');
+Route::middleware(['api', 'admin'])->post('/add-course', 'AdminController@addCourse');
+Route::middleware(['api', 'admin'])->post('/remove-course', 'AdminController@removeCourse');
+Route::middleware(['api', 'admin'])->post('/change-password', 'AdminController@changePassword');
+Route::middleware(['api', 'admin'])->post('/delete-user', 'AdminController@deleteUser');
